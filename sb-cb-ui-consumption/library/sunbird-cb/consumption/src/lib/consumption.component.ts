745acfd94e62ef958e42ce92c7154241a102ba46
Bharath Kumar <bharathkumar@TI-MAC-087.local>
2024-04-25 12:36:56 +0530

    added new package

---
+++ sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/consumption.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sb-uic-consumption',
+  template: `
+    <p>
+      consumption works!
+    </p>
+  `,
+  styles: []
+})
+export class ConsumptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
