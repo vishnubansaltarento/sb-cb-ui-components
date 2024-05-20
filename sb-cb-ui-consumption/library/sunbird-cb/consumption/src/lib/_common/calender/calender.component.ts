6682b8d5d59a7999bc85cb0bc7d8ee04d03c1a99
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2024-05-20 18:01:04 +0530

    Calendar module

--- sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/_common/calender/calender.component.ts
+++ sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/_common/calender/calender.component.ts
@@ -13,14 +13,24 @@ import {
 export class CalenderComponent implements OnInit {
 
   @Input() objectData: any
+  @Input() layoutType: any
+  @Input() fetchDataFromApi: boolean = false
+  isDataLoading: boolean = false
 
   toDay = new Date()
 
   ngOnInit() {
+    if(this.fetchDataFromApi) {
+      this.isDataLoading = true
+      this.getInsiteData()
+    }
 
-    console.log("data ===== ", this.objectData)
-    console.log("toDay ", this.toDay)
-
+  }
+  getInsiteData() {
+    setTimeout(() => {
+      this.isDataLoading = false
+      // tslint:disable-next-line: align
+    }, 5000)
   }
   
  
--- sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/_common/calender/calender.module.ts
+++ sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/_common/calender/calender.module.ts
@@ -2,11 +2,13 @@ import { NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalenderComponent } from './calender.component';
 import { MatIconModule } from '@angular/material';
+import { SkeletonLoaderLibModule } from '../skeleton-loader-lib/skeleton-loader-lib.module';
 
 @NgModule({
   imports: [
     CommonModule,
     MatIconModule,
+    SkeletonLoaderLibModule
   ],
   declarations: [CalenderComponent],
   exports: [CalenderComponent],
