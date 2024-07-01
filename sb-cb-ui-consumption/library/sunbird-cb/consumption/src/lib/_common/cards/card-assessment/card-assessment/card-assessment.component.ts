9366f85dbf4a9bccee5e28322dfb12c742745321
nitinraj-tarento <nitin.raj@tarento.com>
2024-07-01 17:56:02 +0530

    loader issuefix

--- sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/_common/cards/card-assessment/card-assessment/card-assessment.component.ts
+++ sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/_common/cards/card-assessment/card-assessment/card-assessment.component.ts
@@ -81,7 +81,6 @@ export class CardAssessmentComponent implements OnInit {
   }
 
   getRedirectUrlData(contentData: any){
-    console.log(contentData, "contentData========---------------")
     // for telemetry
     if (this.widgetData && this.widgetData.context && this.widgetData.context.pageSection) {
       contentData['typeOfTelemetry'] = this.widgetData.context.pageSection
