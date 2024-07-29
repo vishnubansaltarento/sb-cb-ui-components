eb239a0126b1dd2cb6068321dfbd731cd269c3b5
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2024-07-29 15:09:05 +0530

    Added the comments in URL pipe

--- sb-cb-ui-utils-v2/library/sunbird-cb/utils-v2/src/lib/pipes/pipe-public-URL/pipe-public-URL.pipe.ts
+++ sb-cb-ui-utils-v2/library/sunbird-cb/utils-v2/src/lib/pipes/pipe-public-URL/pipe-public-URL.pipe.ts
@@ -9,11 +9,15 @@ export class PipePublicURL implements PipeTransform {
     this.environment = environment
   }
   transform(value: string): any {
+    // Karmayogi content to be modified with the domain specific url
+    // https://portal.karmayogi.nic.in/content-store/content/do_113918818243969024137/artifact/do_113918818243969024137_1699074493067_effectivecommunication1699074492860.jpg
     if (value.includes("karmayogi")) {
       const mainUrl = value && value.split('/content').pop() || ''
       const finalURL = `${this.environment.contentHost}/${this.environment.contentBucket}/content${mainUrl}`
       return value ? finalURL : ''
     } else {
+      // CIOS content has direct S3 url like blow, so return same url
+      // https://s3.amazonaws.com/ecornell/content/On-Demand/Homepages/Banners/CEPL503-OD3_homepage-banner_v1.jpg
       return value
     }
   }
