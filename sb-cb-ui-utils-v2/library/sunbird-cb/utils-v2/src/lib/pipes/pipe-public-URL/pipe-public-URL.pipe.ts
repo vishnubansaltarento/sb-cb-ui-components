31b92b09c425b98f2f96c00859669e9dda875f84
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2024-07-30 17:41:46 +0530

    Image URL fix

--- sb-cb-ui-utils-v2/library/sunbird-cb/utils-v2/src/lib/pipes/pipe-public-URL/pipe-public-URL.pipe.ts
+++ sb-cb-ui-utils-v2/library/sunbird-cb/utils-v2/src/lib/pipes/pipe-public-URL/pipe-public-URL.pipe.ts
@@ -13,7 +13,7 @@ export class PipePublicURL implements PipeTransform {
 
     // https://portal.karmayogi.nic.in/content-store/content/do_113918818243969024137/artifact/
     // do_113918818243969024137_1699074493067_effectivecommunication1699074492860.jpg
-    if (value.includes('karmayogi')) {
+    if (value && value.includes('karmayogi')) {
       const mainUrl = value && value.split('/content').pop() || ''
       const finalURL = `${this.environment.contentHost}/${this.environment.contentBucket}/content${mainUrl}`
       return value ? finalURL : ''
